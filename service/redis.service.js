41d29b08f6fa0e40d804ffe0594d2b7d0c2cae44
Kubilay <kubilayckmk@gmail.com>
2023-02-07 00:58:35 -0800

    redis test

--- service/redis.service.js
+++ service/redis.service.js
@@ -4,8 +4,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const redisClient = redis.createClient({
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT
+    host: "oregon-redis.render.com",
+    port: "6379"
 });
 
 redisClient.on("connect", () => {
